4448feefa66ff4e771a6d476974affe304a54b2e
Shıttu Adedamola Barry <hokees@Shttus-MacBook-Air.local>
2023-12-28 01:54:35 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+document.getElementById("ski").addEventListener("click",()=>{
+    document.getElementById("yooo").scrollIntoView({behavior:'smooth'})
+})
+
+document.getElementById("con").addEventListener("click",()=>{
+    document.getElementById("yaaa").scrollIntoView({behavior:'smooth'})
+})
+
+document.getElementById("shi").addEventListener("click",()=>{
+    document.getElementById("yiii").scrollIntoView({behavior:'smooth'})
+})
+
+document.getElementById("pro").addEventListener("click",()=>{
+    document.getElementById("yeee").scrollIntoView({behavior:'smooth'})
+})
