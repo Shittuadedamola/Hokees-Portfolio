accefa20418f40cf2b2ae42ad2cb63d0d1779171
Shıttu Adedamola Barry <hokees@Shttus-MacBook-Air.local>
2023-12-29 19:29:17 +0100

    portfolio ready

--- currency calculator.js
+++
@@ -1,9 +0,0 @@
-var NGN = 760.43
-
-// function haha(){
-//   document.getElementById('rate').innerHTML= NGN * document.getElementById("amount").value + document.getElementById("to").value
-// }
-
-function haha(){
-  document.getElementById('rate').innerHTML= NGN * document.getElementById("amount").value + document.getElementById("to").value
-}
